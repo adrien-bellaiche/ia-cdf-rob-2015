0e7f6737038731a7d5531709f4a307130cb74941
simchanu29 <simchanu29@users.noreply.github.com>
2015-05-13 11:44:10 -0700

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -30,6 +30,32 @@ def objectives(side):
     if side == "left":
         pass
         # TODO
+        # claps à droite
+        clap1=Clap([2000,250],1)
+        clap2=Clap([2000,250],1)
+
+        # distributeurs
+        distrib1=Distributeur([30,300],1)
+        distrib2=Distributeur([30,600],1)
+
+        # Plots
+        plot1=Plot([200,90],0)
+        plot2=Plot([200,850],0)
+        plot3=Plot([100,850],0)
+        plot4=Plot([1750,90],0)
+        plot5=Plot([1850,90],0)
+        plot6=Plot([1770,1100],0)
+        plot7=Plot([1400,1300],0)
+        plot8=Plot([1355,870],0)
+
     else:
         # TODO
-        pass
+        # claps à droite
+        clap1=Clap([2000,2750],1)
+        clap2=Clap([2000,2750],1)
+
+        # distributeurs
+        distrib1=Distributeur([30,2700],1)
+        distrib2=Distributeur([30,2400],1)
+
+    return [clap1, clap2, distrib1, distrib2]
