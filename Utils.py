8c6d4f0c2d101070f06bac843c2c5f22a14911ef
adrien-bellaiche <adrien.bellaiche@gmail.com>
2015-05-13 23:25:16 +0200

    4/3/15 update : - Pathfinding finished (testing required) - Global skeleton updated

--- Utils.py
+++ Utils.py
@@ -27,35 +27,41 @@ class Distributeur():
 
 
 def objectives(side):
+    res = []
     if side == "left":
-        pass
-        # TODO
         # claps à droite
-        clap1=Clap([2000,250],1)
-        clap2=Clap([2000,250],1)
+        res.append(Clap([2000, 250], 1))
+        res.append(Clap([2000, 250], 1))
 
         # distributeurs
-        distrib1=Distributeur([30,300],1)
-        distrib2=Distributeur([30,600],1)
+        res.append(Distributeur([30, 300], 1))
+        res.append(Distributeur([30, 600], 1))
 
         # Plots
-        plot1=Plot([200,90],0)
-        plot2=Plot([200,850],0)
-        plot3=Plot([100,850],0)
-        plot4=Plot([1750,90],0)
-        plot5=Plot([1850,90],0)
-        plot6=Plot([1770,1100],0)
-        plot7=Plot([1400,1300],0)
-        plot8=Plot([1355,870],0)
+        res.append(Plot([200, 90], 0))
+        res.append(Plot([200, 850], 0))
+        res.append(Plot([100, 850], 0))
+        res.append(Plot([1750, 90], 0))
+        res.append(Plot([1850, 90], 0))
+        res.append(Plot([1770, 1100], 0))
+        res.append(Plot([1400, 1300], 0))
+        res.append(Plot([1355, 870], 0))
 
     else:
-        # TODO
-        # claps à droite
-        clap1=Clap([2000,2750],1)
-        clap2=Clap([2000,2750],1)
+        res.append(Clap([2000, 2750], 1))
+        res.append(Clap([2000, 2750], 1))
 
         # distributeurs
-        distrib1=Distributeur([30,2700],1)
-        distrib2=Distributeur([30,2400],1)
+        res.append(Distributeur([30, 2700], 1))
+        res.append(Distributeur([30, 2400], 1))
 
-    return [clap1, clap2, distrib1, distrib2]
+        res.append(Plot([200, 90], 0))
+        res.append(Plot([200, 850], 0))
+        res.append(Plot([100, 850], 0))
+        res.append(Plot([1750, 90], 0))
+        res.append(Plot([1850, 90], 0))
+        res.append(Plot([1770, 1100], 0))
+        res.append(Plot([1400, 1300], 0))
+        res.append(Plot([1355, 870], 0))
+
+    return res
--- testFile.py
+++ testFile.py
@@ -1,15 +1,6 @@
 __author__ = 'adrie_000'
 
-#test
-
-
-class Patate():
-    def __init__(self):
-        self.p = "je suis une patate"
-
-    def disp(self):
-        print "banana"
-
-
-p = ["hello", 'c', 3, 3.0125, [Patate(), Patate(), Patate()]]
-p[4][0].disp()
+# test
+res = []
+res.append(3)
+print res
